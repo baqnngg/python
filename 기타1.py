8c5ffc9f222e493a8176679a73505334fa31ccc9
baqnngg <102746289+baqnngg@users.noreply.github.com>
2023-06-23 23:51:45 +0900

    백준 1차원배열 4번문

--- 기타1.py
+++ 기타1.py
@@ -1,4 +1,12 @@
-a = int(input())
-c = list(map(int,input().split()))
-print(min(c),end=" ")
-print(max(c))
+a=[]
+d = 1
+for i in range(9):
+    b = int(input())
+    a.append(b)
+for i in range(9):
+    if a[i] == max(a):
+        break
+    else:
+        d += 1
+print(max(a))
+print(d)
