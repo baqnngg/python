57d755925309ba8638087c11799d83c7318c6e8c
baqnngg <102746289+baqnngg@users.noreply.github.com>
2023-06-24 21:56:47 +0900

    백준 1차원배열 5번문제

--- 기타1.py
+++ 기타1.py
@@ -1,12 +1,8 @@
-a=[]
-d = 1
-for i in range(9):
-    b = int(input())
-    a.append(b)
-for i in range(9):
-    if a[i] == max(a):
-        break
-    else:
-        d += 1
-print(max(a))
-print(d)
+a,b = map(int,input().split())
+c = [0] * a
+for p in range(b):
+    i,j,k = list(map(int,input().split()))
+    for l in range(i-1,j):
+        c[l] = k
+for g in range(a):
+    print(c[g],end=" ")
